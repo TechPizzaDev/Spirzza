356800fa47a8d8cd17c071b585aacd5798da8c80
TechPizza <23627133+TechnologicalPizza@users.noreply.github.com>
2022-03-13 02:33:42 +0100

    WIP: Finishing touches on generation

--- sources/Spirzza/Interop/SpirvCross/SpirvCross.Manual.cs
+++ sources/Spirzza/Interop/SpirvCross/SpirvCross.Manual.cs
@@ -3,9 +3,9 @@ namespace Spirzza.Interop.SpirvCross
 {
     public static unsafe partial class SpirvCross
     {
-		public static int SPVC_MAKE_MSL_VERSION(int major, int minor, int patch) 
-		{
-			return major * 10000 + minor * 100 + patch;
-		}
+        public static int SPVC_MAKE_MSL_VERSION(int major, int minor, int patch) 
+        {
+            return major * 10000 + minor * 100 + patch;
+        }
     }
 }
