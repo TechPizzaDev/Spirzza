03d08299bcc3ac07fbc1f88f9473aee76e90ccda
TechPizza <23627133+TechnologicalPizza@users.noreply.github.com>
2023-12-12 13:49:42 +0100

    Generate sources locally

---
+++ generate_sources.ps1
@@ -0,0 +1,4 @@
+dotnet tool install --global ClangSharpPInvokeGenerator --version 17.0.0
+
+ClangSharpPInvokeGenerator --file-directory submodules/shaderc/libshaderc/include --include-directory submodules/shaderc/libshaderc/include "@scripts/GenerateShaderc.rsp"
+ClangSharpPInvokeGenerator --file-directory submodules/SPIRV-Cross --include-directory submodules/SPIRV-Cross "@scripts/GenerateSpirvCross.rsp"
