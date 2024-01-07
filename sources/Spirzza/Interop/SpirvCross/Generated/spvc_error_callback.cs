2dfe6392fc729ac83a361c27f2d768b79e324ccc
TechPizza <23627133+TechnologicalPizza@users.noreply.github.com>
2024-01-07 01:15:09 +0100

    Remove unused code

--- sources/Spirzza/Interop/SpirvCross/Generated/spvc_error_callback.cs
+++
@@ -1,7 +0,0 @@
-using System.Runtime.InteropServices;
-
-namespace Spirzza.Interop.SpirvCross
-{
-    [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
-    public unsafe delegate void spvc_error_callback(void* userdata, [NativeTypeName("const char *")] sbyte* error);
-}
